76cdf8b9e58aa16ad0d90dd8337aaadcafce5633
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-06-02 18:37:15 +0200

    projet airbnb task 1/2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    const amenities = [];
+    $('input[type=checkbox]').click(function() {
+      if (this.checked) {
+        amenities.push($(this).attr('data-name'));
+      } else {
+        const index = amenities.indexOf($(this).attr('data-name'));
+        amenities.splice(index, 1);
+      }
+      $('.amenities h4').text(amenities.join(', '));
+      });
+  });
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    let listeAmenitiesSelectionnees = {};
+  
+    $('.amenities input[type="checkbox"]').change(function() {
+      if (this.checked) {
+        listeAmenitiesSelectionnees[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete listeAmenitiesSelectionnees[$(this).data('id')];
+      }
+  
+      $('.amenities h4').text(Object.values(listeAmenitiesSelectionnees).join(', '));
+    });
+  
+    $.get("http://127.0.0.1:5001/api/v1/status/")
+      .done(function(data, textStatus) {
+        if (data.status === "OK") {
+          $('#api_status').addClass("available");
+        } else {
+          $('#api_status').removeClass("available");
+        }
+      })
+      .fail(function() {
+        $('#api_status').removeClass("available");
+        alert('get("http://127.0.0.1:5001/api/v1/status/") à échoué');
+      });
+    });
